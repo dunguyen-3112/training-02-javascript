f460a4ca233f0fabb03e0c889e682b8fe3aab7c5
nguyen huu du <dunguyenhuu.it2019@gmail.com>
2022-09-19 08:30:05 +0700

    update view

--- src/scripts/Employees/view.js
+++ src/scripts/Employees/view.js
@@ -1,3 +1,5 @@
+import { Employee } from "./model";
+
 export default class EmployeesView {
     constructor() {
         this.tbody = document.querySelector(".table-body");
@@ -8,44 +10,36 @@ export default class EmployeesView {
         this.form = document.form;
         this.formTitle = document.querySelector(".form-title");
     }
-
-    renderRow = (value, index) =>
-        `<tr class="row-${index}">
+    /**
+     *
+     * @param {Employee} employee
+     * @param {int} index
+     */
+    renderRow = (employee, index) =>
+        `<tr data-id=${employee.id}>
                 <td>${index}</td>
-                <td>${value.name}</td>
-                <td>${value.address}</td>
+                <td>${employee.name}</td>
+                <td>${employee.address}</td>
                 <td>
                     <div class=` +
-        `${value.status ? " active" : "inactive"}>
+        `${employee.status ? " active" : "inactive"}>
                     </div>
                 </td>
                 <td>
-                    <button class="btn btn-warning">Update</button>
+                <img src="./images/icon.png" alt="">
+                    <button class="btn btn-warning"> </button>
                     <button class="btn btn-danger">Delete</button>
                 </td>
             </tr>`;
     /**
      *
-     * @param {Array<Employee>} data
-     * @param {function} renderRow
+     * @param {Array<Employee>} employees
      */
-
-    displayTable(data, handleBtnDelete, handleBtnUpdate) {
-        const rows = data.map((value, index) => this.renderRow(value, index));
+    renderTable(employees) {
+        const rows = employees.map((value, index) =>
+            this.renderRow(value, index)
+        );
         this.tbody.innerHTML = rows.join("");
-        for (let i = 0; i < data.length; i++) {
-            let element = document.querySelector(`.row-${i}`);
-            element
-                .querySelectorAll("button")[1]
-                .addEventListener("click", (e) => {
-                    handleBtnDelete(data[i].id);
-                });
-            element
-                .querySelectorAll("button")[0]
-                .addEventListener("click", (e) => {
-                    handleBtnUpdate(data[i].id);
-                });
-        }
     }
 
     closeModal() {
@@ -55,26 +49,33 @@ export default class EmployeesView {
             this.form.removeAttribute("data-id");
     }
 
-    openModal(title, data) {
+    /**
+     *
+     * @param {string} title
+     * @param {Employee} employee
+     */
+    openModal(title, employee) {
         this.formTitle.innerHTML = title;
         this.form_add.style.display = "block";
         this.btn_close.addEventListener("click", (e) => {
             this.closeModal();
         });
 
-        if (data) {
+        if (employee) {
             this.form.setAttribute("data-id", data.id);
-            document.form.name.value = data.name;
-            document.form.email.value = data.email;
-            document.form.phone.value = data.phone;
-            document.form.address.value = data.address;
-            document.form.status.value = data.status ? "active" : "inactive";
-            document.form.gender.value = data.gender;
+            document.form.name.value = employee.name;
+            document.form.email.value = employee.email;
+            document.form.phone.value = employee.phone;
+            document.form.address.value = employee.address;
+            document.form.status.value = employee.status
+                ? "active"
+                : "inactive";
+            document.form.gender.value = employee.gender;
         }
     }
 
     handleSubmit() {
-        const inputs = {
+        const body = {
             name: document.form.name.value,
             phone: document.form.phone.value,
             address: document.form.address.value,
@@ -86,33 +87,89 @@ export default class EmployeesView {
                 : null,
         };
         this.closeModal();
-        return inputs;
+        return body;
     }
 
-    handleSearch(handle) {
-        this.inputSearch.addEventListener("keyup", (e) => {
-            handle(e.target.value);
-        });
-    }
+    // validateForm() {
+    //     // this.form.name.addEventListener("blur", (e) => {
+    //     //     if (e.target.value.length < 6) {
+    //     //         document.querySelector(".message").style.display = "block";
+    //     //     } else {
+    //     //         document.querySelector(".message").style.display = "none";
+    //     //     }
+    //     // });
+    //     this.form.email.addEventListener("blur", (e) => {
+    //         const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    //         if (!pattern.test(e.target.value))
+    //             this.form.email.parentElement.querySelector(
+    //                 ".message"
+    //             ).style.display = "block";
+    //         else
+    //             this.form.email.parentElement.querySelector(
+    //                 ".message"
+    //             ).style.display = "none";
+    //     });
+    // }
 
-    validateForm() {
-        // this.form.name.addEventListener("blur", (e) => {
-        //     if (e.target.value.length < 6) {
-        //         document.querySelector(".message").style.display = "block";
-        //     } else {
-        //         document.querySelector(".message").style.display = "none";
-        //     }
-        // });
-        this.form.email.addEventListener("blur", (e) => {
-            const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            if (!pattern.test(e.target.value))
-                this.form.email.parentElement.querySelector(
-                    ".message"
-                ).style.display = "block";
-            else
-                this.form.email.parentElement.querySelector(
-                    ".message"
-                ).style.display = "none";
-        });
-    }
+    renderForm = async () => `
+        <div class="form-add-container">
+        <button class="btn-close"></button>
+        <form action="#" method="post" name="form" class="form-add">
+            <title class="form-title">Add new Employee</title>
+            <div class="form-content">
+                <label class="form-2">
+                    <span class="form-label">Name</span>
+                    <input type="text" placeholder="Please type your name" name="name" class="form-control" required>
+                    <span class="message">Valid name!</span>
+                </label>
+                <label>
+                    <span class="form-label">Gender</span>
+                    <div class="form-group">
+                        <div class="form-radio-option">
+                            <span >Male</span>
+                            <input type="radio"  name="gender" class="form-control" value="true" checked>
+                        </div>
+                        <div class="form-radio-option">
+                            <span >FeMale</span>
+                            <input type="radio"  name="gender" class="form-control" value="false">
+                        </div>
+                    </div>
+                </label>
+                <label>
+                    <span class="form-label">Status</span>
+                    <select name="status" class="form-control">
+                        <option value="active">Active</option>
+                        <option value="inactive">In Active</option>
+                    </select>
+                </label>
+                <label class="form-2">
+                    <span class="form-label">Address</span>
+                    <input type="text" placeholder="Please type your address  " name="address" class="form-control" required>
+                    <span class="message"></span>
+                </label>
+                <label class="form-2">
+                    <span class="form-label">Email</span>
+                    <input type your="email"  placeholder="Please type your email  " name="email" class="form-control" required>
+                    <span class="message">Valid email!</span>
+                </label>
+                <label class="form-2">
+                    <span class="form-label">Phone</span>
+                    <input type="text" placeholder="Please type your phone  " name="phone" class="form-control" required>
+                    <span class="message"></span>
+                </label>
+            </div>
+            <div class="form-action">
+                <button type="reset" class="btn btn-dark" name="btnReset">Reset</button>
+                <button type="submit" class="btn btn-dark" name="btnSave">Save</button>
+            </div>
+        </form>
+        </div>
+    `;
+
+    renderFormSearch = () => `
+        <form class="form-search" name="formSearch" action="/search" method="post">
+            <input type="text" class="control-search" placeholder="Search...">
+            <button class="icon-search" type="submit"></button>
+        </form>
+    `;
 }
