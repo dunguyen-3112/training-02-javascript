0d873ffefc302b1f477253d5896ba8f00d383ab6
nguyen huu du <dunguyenhuu.it2019@gmail.com>
2022-09-19 08:32:58 +0700

    helper file

---
+++ src/scripts/helpers/api-helper.js
@@ -0,0 +1,23 @@
+import { API_BASE } from "../constant";
+
+export default class API_Helper {
+    constructor() {}
+    /**
+     *
+     * @param {string} url
+     * @param {*} option
+     * @returns
+     */
+    fetchAPI = async ({ url, data, method }) => {
+        if (data) {
+            const option = {
+                method,
+                body: JSON.stringify(data),
+                headers: { "Content-Type": "application/json" },
+            };
+            return await fetch(`${url}`, option).then((res) => res.json());
+        }
+
+        return await fetch(`${url}`).then((res) => res.json());
+    };
+}
