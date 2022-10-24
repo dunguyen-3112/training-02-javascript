0b70cf614a3c5fe026aaf2c64eeb7a7ec7a48599
Dunguyen-3112 <dunguyenhuu.it2019@gmail.com>
2022-10-24 15:51:17 +0700

    fix avatar null

--- src/scripts/employees/view.js
+++ src/scripts/employees/view.js
@@ -63,7 +63,7 @@ export default class EmployeesView {
             `<tr data-id=${employee.id}>
                         <td>${index}</td>
                         <td><img src="${
-                            employee.avatar.localeCompare("") === 0
+                            !employee.avatar
                                 ? "https://media.istockphoto.com/vectors/user-icon-flat-isolated-on-white-background-user-symbol-vector-vector-id1300845620?k=20&m=1300845620&s=612x612&w=0&h=f4XTZDAv7NPuZbG0habSpU0sNgECM0X7nbKzTUta3n8="
                                 : employee.avatar
                         }" alt="avatar"></td>
